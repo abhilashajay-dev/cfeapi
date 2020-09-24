d7440d6ffb490a47930956eb15f91cd1f95e30d5
abhilashajay-dev <55262545+abhilashajay-dev@users.noreply.github.com>
2020-09-24 19:10:07 +0530

    common end point created for model

--- scripts/cfe_pure_api.py
+++ scripts/cfe_pure_api.py
@@ -7,17 +7,31 @@ BASE_URL = " http://127.0.0.1:8000/"
 ENDPOINT = "api/updates/"
 
 
-def get_list():
-    r = requests.get(BASE_URL + ENDPOINT)
-    data = r.json()
-    for obj in data:
-    	if obj['id']==2:
-	    	r2 = requests.get(BASE_URL + ENDPOINT + str(obj['id']))
-	    	print(f'Object called using Object ID :',r2.json(),'\n')
-	    	# data = r2.json()
-	    	# print(dir(r2))
-    return data
+# def get_list():
+#     r = requests.get(BASE_URL + ENDPOINT)
+#     data = r.json()
+#     for obj in data:
+#     	if obj['id']==2:
+# 	    	r2 = requests.get(BASE_URL + ENDPOINT + str(obj['id']))
+# 	    	print(f'Object called using Object ID :',r2.json(),'\n')
+# 	    	# data = r2.json()
+# 	    	# print(dir(r2))
+#     return data
 
+
+def get_list(id=None):
+	new_data ={
+	'id':id,
+	}
+	if new_data['id'] == '' or new_data['id'] == "":
+		new_data['id'] = None
+	data = json.dumps(new_data)
+	
+	r = requests.get(BASE_URL + ENDPOINT, data=data)
+	if r.status_code == requests.codes.ok : 
+		return r.json()
+	return r.text    
+    	
 
 def create_update():
 	new_data= {
@@ -37,10 +51,11 @@ def create_update():
 
 def do_obj_update():
 	new_data= {
+	'id':3,
 	'content':"Update from do_obj_update"
 	}
 
-	r = requests.put(BASE_URL + ENDPOINT + "2/", data=json.dumps(new_data))
+	r = requests.put(BASE_URL + ENDPOINT , data=json.dumps(new_data))
 	# print(r.json())
 	# print(r.headers)
 	print(r.status_code)
@@ -54,10 +69,11 @@ def do_obj_update():
 
 def do_obj_delete():
 	new_data= {
+	'id':3,
 	'content':"Delete from do_obj_update"
 	}
 
-	r = requests.delete(BASE_URL + ENDPOINT + "2/")
+	r = requests.delete(BASE_URL + ENDPOINT, data=json.dumps(new_data))
 	# print(r.json())
 	# print(r.headers)
 	print(r.status_code)
@@ -69,7 +85,7 @@ def do_obj_delete():
 
 
 
-# print(get_list())
+print(get_list())
 # print(create_update())
-print(do_obj_update())
+# print(do_obj_update())
 # print(do_obj_delete())	
--- updates/api/views.py
+++ updates/api/views.py
@@ -75,6 +75,10 @@ class UpdateModelDetailAPIView(HttpResponseMixin, CSRFExemptMixin, View):  # ret
         return self.render_to_response(json_data)
 
 
+
+#Auth / Permission ---Django REST FRAMEWORK
+
+
 class UpdateModelListAPIView(HttpResponseMixin, CSRFExemptMixin, View):  # list & create
 
 
@@ -83,11 +87,33 @@ class UpdateModelListAPIView(HttpResponseMixin, CSRFExemptMixin, View):  # list 
 
     is_json = True
 
+
+
+    def get_obj(self, id=None):
+        qs = UpdateModel.objects.filter(id=id)
+        if qs.count()==1:
+            return qs.first()
+        return None    
+
     def get(self, request, *args, **kwargs):
-        qs = UpdateModel.objects.all()
-        json_data = qs.serialize()
-        # return HttpResponse(json_data, content_type='application/json')
-        return self.render_to_response(json_data)
+        passed_data = json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+
+        if passed_id is not None:
+            obj = self.get_obj(id=passed_id)
+            if obj  is None:
+                error_data = json.dumps({'message':'Update not found'})
+                return self.render_to_response(error_data, status=404)
+            json_data = obj.serialize()
+            # return HttpResponse(json_data, content_type='application/json')
+            return self.render_to_response(json_data)
+
+
+        else:
+            qs = UpdateModel.objects.all()
+            json_data = qs.serialize()
+            # return HttpResponse(json_data, content_type='application/json')
+            return self.render_to_response(json_data)
 
     def post(self, request, *args, **kwargs):
         valid_json=is_json(request.body)
@@ -111,8 +137,64 @@ class UpdateModelListAPIView(HttpResponseMixin, CSRFExemptMixin, View):  # list 
         # return HttpResponse(json_data, content_type='application/json', status=400)
         return self.render_to_response(json_data, status=400)
 
+    # def delete(self, request, *args, **kwargs):
+    #     json_data = json.dumps( {"message":"you cannot delete an entire list"})
+    #     status_code = 403 #Forbidden
+    #     # return HttpResponse(json_data, content_type='application/json', status=status_code)
+    #     return self.render_to_response(json_data, status=status_code)
+
+    def put(self, request, *args, **kwargs):
+        valid_json=is_json(request.body)
+        if not valid_json:
+            error_data = json.dumps({'message':'Invalid data sent please send using JSON'})
+            return self.render_to_response(error_data, status=400)
+        passed_data = json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+
+
+        obj = self.get_obj(id=passed_id)
+        if obj is None:
+            error_data = json.dumps({'message':'object not found'})
+            return self.render_to_response(error_data, status=404)
+        # return HttpResponse(json_data, content_type='application/json')
+        # print(dir(request))
+        # print(request.body)
+
+        passed_data = json.loads(request.body)
+        data = json.loads(obj.serialize())
+        for key, value in passed_data.items():
+            data[key] = value
+        print(passed_data)
+
+        form = UpdateModelForm(data, instance=obj)
+        if form.is_valid():
+            obj = form.save(commit=True)
+            obj_data = obj.serialize()
+            return self.render_to_response(obj_data, status=201)
+        if form.errors:
+            data = json.dumps(form.errors)
+            return self.render_to_response(data , status=400)     
+
+        json_data = json.dumps({'message':'Something is new'})
+        return self.render_to_response(json_data)
+
     def delete(self, request, *args, **kwargs):
-        json_data = json.dumps( {"message":"you cannot delete an entire list"})
-        status_code = 403 #Forbidden
-        # return HttpResponse(json_data, content_type='application/json', status=status_code)
-        return self.render_to_response(json_data, status=status_code)
+        valid_json=is_json(request.body)
+        if not valid_json:
+            error_data = json.dumps({'message':'Invalid data sent please send using JSON'})
+            return self.render_to_response(error_data, status=400)
+        passed_data = json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+
+
+        obj = self.get_obj(id=passed_id)
+        if obj is None:
+            error_data = json.dumps({'message':'object not found'})
+            return self.render_to_response(error_data, status=404)
+
+        # json_data = obj.serialize()
+        message = f"{obj} got deleted"
+        obj.delete()
+        json_data=json.dumps({'message':message})
+        # return HttpResponse(json_data, content_type='application/json')
+        return self.render_to_response(json_data)
