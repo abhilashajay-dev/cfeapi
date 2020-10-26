8842baab7b532a80fc348f5684fb6c75ce8435dc
abhilashajay-dev <55262545+abhilashajay-dev@users.noreply.github.com>
2020-10-26 07:39:45 +0530

    LimitOffsetPagination

--- cfeapi/restconf/pagination.py
+++ cfeapi/restconf/pagination.py
@@ -1,4 +1,9 @@
 from rest_framework import pagination
 
-class CustomPagination(pagination.PageNumberPagination):
-	page_size = 5 
+# class CustomPagination(pagination.PageNumberPagination):
+# 	page_size = 5 
+
+
+class CustomPagination(pagination.LimitOffsetPagination):
+	default_limit = 5
+	max_limit = 10
